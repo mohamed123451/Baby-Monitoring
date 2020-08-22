a3cfaba22a4d146e311656d3986fc52322c10b0d
bess project <projectbess0@gmail.com>
2020-08-22 10:04:07 +0200

    sending Messages and handling functions

--- Baby_Monitoring/Baby_Monitoring.ino
+++ Baby_Monitoring/Baby_Monitoring.ino
@@ -7,42 +7,117 @@
  *                - by that we can be notified when the baby is awake, and check on him, then we need to reset
  */
 
-#define SoundSensorPin      2
-#define PIRMotionSensorPin  4
-#define BuzzerPin           5
+#include <SoftwareSerial.h>
 
-int SoundSensorValue = 0;
-int PIRMotionSensorValue = 0;
+
+#define SoundSensorPin      3
+#define PIRMotionSensorPin  4
+#define LedPin              13
+
+int   SoundSensorValue      = 0;
+int   PIRMotionSensorValue  = 0;
+bool  isCrying              = false;
+bool  isMoving              = false;
+bool  notReset              = true;
+
+String   phone_number1 = "+ZZxxxxxxxxxx";    //change ZZ with country code and xxxxxxxxxxx with phone number to sms
+
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(D3, D2); //SIM800L Tx & Rx is connected to Arduino #D3 & #D2
+
 
 void setup() {
+
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+
   pinMode(SoundSensorPin,     INPUT);
   pinMode(PIRMotionSensorPin, INPUT);
-  pinMode(BuzzerPin,     OUTPUT);
+  pinMode(LedPin,             OUTPUT);
+  pinMode(LedPin,             OUTPUT);
 }
 
 void loop() {
-  SoundSensorValue = digitalRead(SoundSensorPin);
-  PIRMotionSensorValue = digitalRead(PIRMotionSensorPin);
+  
+  checkTheBabeyOnce();  
 
+}
+
+
+// Sending SMS via Sim800
+void sendSMS(String message, String number) 
+{ 
+  String mnumber = "AT + CMGS = \""+number+"\"";
+  mySerial.print("AT+CMGF=1\r");             
+  delay(80);
+  mySerial.println(mnumber);                                    
+  delay(80);
+  mySerial.println(message);       
+  delay(80);
+  mySerial.println((char)26);                       
+  delay(80);
+  mySerial.println();
+  delay(100);
+  Serial.print("sending message to");                                
+}
+
+
+bool checkCrying()
+{
+  SoundSensorValue = digitalRead(SoundSensorPin);
   if(SoundSensorValue == HIGH)
   {
-    digitalWrite(BuzzerPin, HIGH);
+    return true;
   }
   else{
-    digitalWrite(BuzzerPin, LOW);
+    return false;
   }
+}
 
-  
+void sendCryingMessage()
+{
+  digitalWrite(LedPin, HIGH);
+  sendSMS("the baby is crying", phone_number1); 
+}
+
+bool checkMotion()
+{
+  PIRMotionSensorValue = digitalRead(PIRMotionSensorPin);
   if(PIRMotionSensorValue == HIGH)
   {
-    delay(500);
+    delay(5000);
     PIRMotionSensorValue = digitalRead(PIRMotionSensorPin);
     if(PIRMotionSensorValue == HIGH)
     {
-      digitalWrite(BuzzerPin, HIGH);
+      return true;
     }
     else{
-      digitalWrite(BuzzerPin, LOW);
+      return false;
+    }
+  }
+}
+
+void sendMotionMessage()
+{
+  digitalWrite(LedPin, HIGH);
+  sendSMS("the baby is moving", phone_number1);
+}
+
+void checkTheBabeyOnce()
+{
+  while(notReset)
+  {
+    isCrying = checkCrying();
+    isMoving = checkMotion();
+  
+    if(isCrying)
+    {
+      sendCryingMessage();
+      notReset = false;
+    }
+    if(isMoving)
+    {
+      sendMotionMessage();
     }
   }
 }
